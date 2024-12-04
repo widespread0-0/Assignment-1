76df2a3032972a05d3de3d9bf092fb790914d1fc
widespread0-0 <danielolugbule@gmail.com>
2024-12-04 14:26:19 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    echo "Hello programming<br>";
+    echo "This is a new line<br>";
+    echo $name= "Daniel";
+    echo "?<br>";
+    echo "$name is a bodacious guy<br>";
+    echo $age = 16;
+    echo $gender= "Male<br>";
+    
+    $fruits = array("apple", "Banana", "Cherry", "Mango", "Guava");
+    echo var_dump($fruits)."<br>";
+    echo json_encode($fruits);
+?>
